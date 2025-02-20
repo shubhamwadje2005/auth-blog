797bc050b87e8f8eaf9a9174f316a0ddeb1cbd71
SHUBHAM WADJE <shubhamwadje2005@gmail.com>
2025-02-20 15:34:15 +0530

    frontend added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const path = require("path")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const { userProtected, adminProtected } = require("./middleware/auth.middleware")
@@ -7,14 +8,15 @@ require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "https://auth-blog-qk5j.onrender.com", credentials: true }))
 app.use(cookieParser())
-
+app.use(express.static("dist"))
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/user", userProtected, require("./routes/blog.routes"))
 app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found !" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "resource not found !" })
 })
 
 app.use((err, req, res, next) => {
